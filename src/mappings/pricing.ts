2230fed29a3d6ae1dfe4b34ecc3568541ef0532e
Mulenga Bowa <mulengabowa53@gmail.com>
2020-12-27 21:36:19 +0200

    added new algo

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -21,21 +21,19 @@ export function getEthPriceInUSD(): BigDecimal {
     let daiWeight = daiPair.reserve0.div(totalLiquidityETH)
     let usdcWeight = usdcPair.reserve0.div(totalLiquidityETH)
     let usdtWeight = usdtPair.reserve0.div(totalLiquidityETH)
-    let price = daiPair.token1Price
+    return daiPair.token0Price
       .times(daiWeight)
-      .plus(usdcPair.token1Price.times(usdcWeight))
-      .plus(usdtPair.token1Price.times(usdtWeight))
-    return ONE_BD.div(price)
+      .plus(usdcPair.token0Price.times(usdcWeight))
+      .plus(usdtPair.token0Price.times(usdtWeight))
     // dai and USDC have been created
   } else if (daiPair !== null && usdcPair !== null) {
     let totalLiquidityETH = daiPair.reserve0.plus(usdcPair.reserve0)
     let daiWeight = daiPair.reserve0.div(totalLiquidityETH)
     let usdcWeight = usdcPair.reserve0.div(totalLiquidityETH)
-    let price = daiPair.token1Price.times(daiWeight).plus(usdcPair.token1Price.times(usdcWeight))
-    return ONE_BD.div(price)
+    return daiPair.token0Price.times(daiWeight).plus(usdcPair.token0Price.times(usdcWeight))
     // USDC is the only pair so far
   } else if (usdcPair !== null) {
-    return ONE_BD.div(usdcPair.token1Price)
+    return usdcPair.token0Price
   } else {
     return ZERO_BD
   }
